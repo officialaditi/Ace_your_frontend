12eb0d128e99f40d5ddb8b9205f612ae80c0667f
Aditi Gaikwad <aditigaikwad0110@gmail.com>
2025-05-06 14:16:42 +0530

    'change in answer ui'

--- src/Screens/SingleTheoryQuestionScreen.jsx
+++ src/Screens/SingleTheoryQuestionScreen.jsx
@@ -1,14 +1,9 @@
 import { useParams, Link } from "react-router-dom";
 import { Theory_Questions } from "../data/TheoryQuestion";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 
 const SingleTheoryQuestionScreen = () => {
-  const [showAns, setShowAns] = useState(false);
-  const handleAnsToggle = () => {
-    setShowAns(!showAns);
-  };
-
   const { id } = useParams();
 
   const theory = Theory_Questions.find((theory) => theory.id === Number(id));
@@ -27,32 +22,10 @@ const SingleTheoryQuestionScreen = () => {
           <FaArrowLeftLong />
           Back to JavaScript Question
         </Link>
-        <div className=" text-white flex flex-col gap-4 mx-10 bg-slate-700 rounded-md px-5 py-3">
-          <div className="flex  items-center justify-between">
-            <h1 className="flex items-center text-base lg:text-2xl gap-3">
-              Theory Questions
-            </h1>
-            <span className="bg-slate-800 rounded-full px-4 py-1">
-              {theory.topic}
-            </span>
-          </div>
-
-          <h3 className="text-xl">{theory.question}</h3>
-
-         <div className="flex items-end justify-end">
-         <button
-            className="bg-blue-600 hover:bg-blue-700 text-lg lg:text-xl px-3 py-1 rounded-xl w-fit flex items-center gap-3"
-            onClick={handleAnsToggle}
-          >
-            {showAns ? "Hide Answer" : "Show Answer"}
-          </button>
-         </div>
-          {showAns && (
-            <div>
-              <h1 className="text-2xl">{theory.answer}</h1>
-            </div>
-          )}
+        <div className="bg-slate-700 text-white rounded-md h-40 mx-10 flex items-center justify-center">
+          <h2 className="text-semibold text-3xl mx-10 py-2  ">{theory.answer}</h2>
         </div>
+
         <div className="text-white mt-10 w-1/2 flex flex-col gap-4 mx-10 bg-slate-700 rounded-md px-5 py-3">
           <h1 className="font-bold text-3xl">Resources:-</h1>
           <h3 className="font-semibold">Useful Links:</h3>
